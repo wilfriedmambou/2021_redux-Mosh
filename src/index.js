360073460bae1150cbc302ef1c7f1de801dae771
wilfriedmambou <wilfriedmambou300@gmail.com>
2021-09-16 21:23:12 +0100

    version de redux sans la separation des constantes pour les types

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn mo re: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+import store from './store/store'
+
+const unsubscribe = store.subscribe (()=>{
+  console.log("Store Changed" , store.getState())
+})
+
+store.dispatch({
+type:"bugAdded",
+payload:{
+  description:"Bug1"
+}
+});
+
+unsubscribe()
+
+store.dispatch({
+  type:"bugRemoved",
+  payload:{
+  id:1
+  }
+  });
+console.log(store.getState());
